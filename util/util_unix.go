b4c4525fe1a8c9b5b9646eb235459a1f2b09fcfd
kants <real.9593@163.com>
2023-12-22 00:10:51 +0800

    fix:获取磁盘码 bugfix

--- util/util_unix.go
+++ util/util_unix.go
@@ -2,27 +2,28 @@
 // +build unix
 
 package util
-  
-import (  
- "fmt"  
- "os"  
- "syscall"  
-)  
-  
-func GetDiskCode() (string, error){  
-	// 获取根目录的文件信息  
-	fileInfo, err := os.Stat("/")  
-	if err != nil {  
-		fmt.Println("无法获取根目录的文件信息：", err)  
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"syscall"
+)
+
+func GetDiskCode() (string, error) {
+	// 获取根目录的文件信息
+	fileInfo, err := os.Stat("/")
+	if err != nil {
+		fmt.Println("无法获取根目录的文件信息：", err)
 		return "", err
-	}  
-	 
-	// 转换为系统相关的文件信息  
-	sysInfo := fileInfo.Sys().(*syscall.Stat_t)  
-	 
-	// 获取磁盘唯一ID  
-	diskID := sysInfo.Dev  
-	 
+	}
+
+	// 转换为系统相关的文件信息
+	sysInfo := fileInfo.Sys().(*syscall.Stat_t)
+
+	// 获取磁盘唯一ID
+	diskID := sysInfo.Dev
+
 	// fmt.Printf("磁盘唯一ID：%d\n", diskID)
-	return diskID, nil
+	return strconv.FormatUint(diskID, 10), nil
 }
